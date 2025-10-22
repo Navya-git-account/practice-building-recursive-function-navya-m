70f4e2e4092c7f78eea58ea8c5e788a7c162d553
Navya <navyajune01@gmail.com>
2025-10-22 14:45:54 -0500

    Create reverseString using recursion

--- recursive-function.js
+++ recursive-function.js
@@ -1,13 +1,19 @@
-function reverseString(inputstring){
-    if (inputstring.length <= 1 ){
-
+// Function to reverse a string using recursion
+function reverseString(inputstring) {
+    
+    // Base case: if the string has 0 or 1 character, return it as-is
+    if (inputstring.length <= 1) {
         return inputstring;
     }
 
+    // Recursive case:
+    // Take the last character and append the reversed result of the remaining string
     return inputstring.charAt(inputstring.length - 1) + reverseString(inputstring.slice(0, -1));
-
 }
-console.log(reverseString("hello"));
-console.log(reverseString("recursion"));
-console.log(reverseString("a"));
-console.log(reverseString(""));
+
+// example usecases
+
+console.log(reverseString("hello")); // output : olleh
+console.log(reverseString("recursion")); // output : noisrucer
+console.log(reverseString("a")); // output : a
+console.log(reverseString("")); // output : 
